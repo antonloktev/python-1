7557fb9621ca0b96c7d6b87c3d2ec3ed02cf2af9
Anton <antonios113loktev@gmail.com>
2018-07-09 20:46:59 +0300

    Исправлен вывод
    Выражение, которое необходимо вывести, взято в скобки согласно синтаксису print(). Исправлены кавычки: кавычки должны быть парными, поэтому двойная кавычка в конце строки заменена на одинарную, как в начале строки.

--- task-3/task-3.py
+++ task-3/task-3.py
@@ -7,4 +7,4 @@ def compute(n):
         i += 1
     return s/n
 n = int(sys.argv[1])
-print 'average of %d random numbers is %g" % (n, compute(n))
+print('average of %d random numbers is %g' % (n, compute(n)))
